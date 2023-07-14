65dfeedc9729eb918a3b600503409db4e14bf8d2
madeleine-capper <madeleine@codeup.com>
2023-07-14 15:42:50 -0500

    add control structures lesson content

---
+++ control_structures.py
@@ -0,0 +1,50 @@
+'''
+Python Lesson #3:
+
+Control Structures:
+'''
+
+# if statements, for loops, while loops
+
+# an if statement is going to control an action associated with a truth value
+
+# if __thing that is true___, do this action
+
+
+# lets create a variable
+
+my_class_greeting = 'hello class!'
+
+if my_class_greeting == 'hello class!':
+	print('hi!!')
+
+#an option that is not true:
+if my_class_greeting == 'hi class':
+	print('hi!')
+
+# WHILE LOOPS:
+# if the thing is true:
+# do the action
+# as long as the thing is true:
+# continue doing the action
+# if the tthing is false:
+#please stop the action
+
+# initialize this variable my_response as an empty string
+my_response = ''
+
+# start a while loop 
+# using an input function call:
+
+while my_response != 'please stop':
+	print('hey whats up happy friday')
+
+
+
+
+
+
+
+
+
+
